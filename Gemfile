6ea73d36c9d045d9b7f45d5c4092b830c8d71353
Glen Kaukola <gkaukola@cs.ucr.edu>
2012-12-18 15:09:02 -0800

    First commit.

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+
+group :development do
+  gem 'sqlite3'
+  gem 'therubyracer'
+end
+
+gem 'json'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'execjs'
+
+group :production do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = 'dcedc85e37946448dbdc8c57142a769066aae9d4c3fab5621d0d7dde023a061cb7bee2dce9721439a4321a852f3ebc0d5e14e60747aefc46d9f385a8a7cbbd64'
